e1af5b879e36eca93e638042569fdad2d114bd70
Noor-ul-Hassan <hafiznoorulhassan987@gmail.com>
2024-09-02 12:50:47 +0500

    aboutus changes

--- src/components/Aboutus.jsx
+++ src/components/Aboutus.jsx
@@ -7,7 +7,7 @@ function Aboutus() {
     <div id='aboutus-Container'>
       <h2>ABOUT ORGANICO</h2>
       <p>Welcome to our website! We are your trusted organic oil seller, providing high-quality and pure oils for your wellness needs. Our products are carefully sourced and produced, ensuring they are free from harmful chemicals and additives. Discover a wide range of organic oils, including cold-pressed oils, essential oils, and carrier oils. Each product comes with detailed information about its origin, benefits, and uses to help you make informed choices. Experience the natural goodness of our oils, which are extracted using eco-friendly methods, preserving the integrity of the ingredients. Whether you're looking for oils to enhance your skincare routine, support your overall health, or simply indulge in aromatherapy, we have the perfect solution for you. We prioritize sustainability and environmental responsibility to promote ethical practices. When you choose our organic oils and other products, you are not only benefiting yourself but also contributing to a healthier planet. Join us on this journey of wellness and discover the power of nature in its purest form. Explore our collection and find the perfect organic oil for your needs today!</p>
-   <button>Shop Our Collection</button>
+   <button>Shop Our Collections</button>
     </div>
     </div>
   );
